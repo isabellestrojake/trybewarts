2bf87dcad49d4e56cd8a4ec82549b7ddc08d5618
Isabelle Strojake <strojake@gmail.com>
2022-05-05 17:01:57 -0300

    consertado 3 e feito 13

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const email = document.querySelector('#email').value;
-const senha = document.querySelector('#senha').value;
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
 
 document.querySelector('#login').addEventListener('click', () => {
-  if (email === 'tryber@teste.com' && senha === '123456') {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
