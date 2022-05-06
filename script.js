b205f7ea6a3107f51fda032afa1f0e96f08567d7
Thaísy <thaisy-ms@outlook.com>
2022-05-06 17:57:00 -0300

    requisito 21 finalizado

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ document.querySelector('#login').addEventListener('click', () => {
 
 // Desabilita/habilita botão Enviar
 
-const input = document.getElementById('submit-btn');
-input.disabled = true;
+const botaoEnviar = document.getElementById('submit-btn');
+botaoEnviar.disabled = true;
 
 const checkbox = document.getElementById('agreement');
 
 function desabilitaBotao() {
   if (checkbox.checked === true) {
-    input.disabled = false;
+    botaoEnviar.disabled = false;
   } else {
-    input.disabled = true;
+    botaoEnviar.disabled = true;
   }
 }
 checkbox.addEventListener('input', desabilitaBotao);
@@ -44,17 +44,55 @@ desc.addEventListener('input', quantidadeCaracteres);
 
 const primeiroNome = document.querySelector('#input-name')
 const sobrenome = document.querySelector('#input-lastname')
-const emailCadastrado = document.querySelector('#email')
+const emailCadastrado = document.querySelector('#input-email')
 const casaEscolhida = document.querySelector('#house')
-const familiaEscolhida = document.querySelectorAll('#label-family')
-const materiaEscolhida = document.querySelectorAll('#label-family')
+const familiaEscolhida = document.querySelectorAll('input[type=radio]')
+const materiaEscolhida = document.querySelectorAll('.subject')
+const avaliacao = document.querySelectorAll('.avaliacao')
+const observacoes = document.querySelector("#textarea")
+const form = document.querySelector('#evaluation-form')
 
+function substituiFormulario() {
+  form.innerText = '';
+  const novaDiv = document.createElement('div')
+  form.appendChild(novaDiv)
 
-const main = document.querySelector('#main')
-input.addEventListener('click', mostraInformacoes)
-
-function mostraInformacoes() {
   const nomeCompleto = document.createElement('p')
-  nomeCompleto.innerText = 'Nome: ' + primeiroNome.value + ' ' + sobrenome.value
-  primeiroNome.insertBefore(nomeCompleto, sobrenome)
+  nomeCompleto.innerHTML = 'Nome: ' + primeiroNome.value + ' ' + sobrenome.value + '<br>' + 'Email: ' + emailCadastrado.value + '<br>' +
+    'Casa: ' + casaEscolhida.value + '<br>' + 'Família: ' + valorDoInputRadio() + '<br>' + 'Matérias: ' + valorDoInputCheckbox() + '<br>' +
+    'Avaliação: ' + valorRadioAvaliacao() + '<br>' + 'Observações: ' + observacoes.value
+
+  novaDiv.appendChild(nomeCompleto)
 }
+
+function valorDoInputRadio() {
+  const inputRadioChecked = [];
+  for (let i = 0; i < familiaEscolhida.length; i += 1) {
+    if (familiaEscolhida[i].checked) {
+      inputRadioChecked.push(familiaEscolhida[i].value)
+    }
+  }
+  return inputRadioChecked
+}
+
+function valorDoInputCheckbox() {
+  const inputCheckbox = [];
+  for (let i = 0; i < materiaEscolhida.length; i += 1) {
+    if (materiaEscolhida[i].checked) {
+      inputCheckbox.push(materiaEscolhida[i].value)
+    }
+  }
+  return inputCheckbox.join(', ')
+}
+
+function valorRadioAvaliacao() {
+  const inputCheckedAvaliacao = [];
+  for (let i = 0; i < avaliacao.length; i += 1) {
+    if (avaliacao[i].checked) {
+      inputCheckedAvaliacao.push(avaliacao[i].value)
+    }
+  }
+  return inputCheckedAvaliacao
+}
+
+botaoEnviar.addEventListener('click', substituiFormulario)
