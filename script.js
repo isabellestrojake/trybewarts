1910ddcdaaf56da8c0d6bd65c377c7533ad38466
Thaísy <thaisy-ms@outlook.com>
2022-05-06 18:32:29 -0300

    erros de lint corrigidos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Verifica as informações de login
+//  Verifica as informações de login
 
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
@@ -40,59 +40,58 @@ desc.addEventListener('input', quantidadeCaracteres);
 
 // A função do counter foi criada com base em: https://medium.com/walternascimentobarroso-pt/contador-de-caracteres-com-html-e-js-648ee612d7be
 
-//Mostra as informações ao submeter o formulário
+// Mostra as informações ao submeter o formulário
 
-const primeiroNome = document.querySelector('#input-name')
-const sobrenome = document.querySelector('#input-lastname')
-const emailCadastrado = document.querySelector('#input-email')
-const casaEscolhida = document.querySelector('#house')
-const familiaEscolhida = document.querySelectorAll('input[type=radio]')
-const materiaEscolhida = document.querySelectorAll('.subject')
-const avaliacao = document.querySelectorAll('.avaliacao')
-const observacoes = document.querySelector("#textarea")
-const form = document.querySelector('#evaluation-form')
-
-function substituiFormulario() {
-  form.innerText = '';
-  const novaDiv = document.createElement('div')
-  form.appendChild(novaDiv)
-
-  const nomeCompleto = document.createElement('p')
-  nomeCompleto.innerHTML = 'Nome: ' + primeiroNome.value + ' ' + sobrenome.value + '<br>' + 'Email: ' + emailCadastrado.value + '<br>' +
-    'Casa: ' + casaEscolhida.value + '<br>' + 'Família: ' + valorDoInputRadio() + '<br>' + 'Matérias: ' + valorDoInputCheckbox() + '<br>' +
-    'Avaliação: ' + valorRadioAvaliacao() + '<br>' + 'Observações: ' + observacoes.value
-
-  novaDiv.appendChild(nomeCompleto)
-}
+const primeiroNome = document.querySelector('#input-name');
+const sobrenome = document.querySelector('#input-lastname');
+const emailCadastrado = document.querySelector('#input-email');
+const casaEscolhida = document.querySelector('#house');
+const familiaEscolhida = document.querySelectorAll('input[type=radio]');
+const materiaEscolhida = document.querySelectorAll('.subject');
+const avaliacao = document.querySelectorAll('.avaliacao');
+const observacoes = document.querySelector('#textarea');
+const form = document.querySelector('#evaluation-form');
 
 function valorDoInputRadio() {
   const inputRadioChecked = [];
   for (let i = 0; i < familiaEscolhida.length; i += 1) {
     if (familiaEscolhida[i].checked) {
-      inputRadioChecked.push(familiaEscolhida[i].value)
+      inputRadioChecked.push(familiaEscolhida[i].value);
     }
   }
-  return inputRadioChecked
+  return inputRadioChecked;
 }
 
 function valorDoInputCheckbox() {
   const inputCheckbox = [];
   for (let i = 0; i < materiaEscolhida.length; i += 1) {
     if (materiaEscolhida[i].checked) {
-      inputCheckbox.push(materiaEscolhida[i].value)
+      inputCheckbox.push(materiaEscolhida[i].value);
     }
   }
-  return inputCheckbox.join(', ')
+  return inputCheckbox.join(', ');
 }
 
 function valorRadioAvaliacao() {
   const inputCheckedAvaliacao = [];
   for (let i = 0; i < avaliacao.length; i += 1) {
     if (avaliacao[i].checked) {
-      inputCheckedAvaliacao.push(avaliacao[i].value)
+      inputCheckedAvaliacao.push(avaliacao[i].value);
     }
   }
-  return inputCheckedAvaliacao
+  return inputCheckedAvaliacao;
 }
 
-botaoEnviar.addEventListener('click', substituiFormulario)
+function substituiFormulario() {
+  form.innerText = '';
+  const novaDiv = document.createElement('div');
+  form.appendChild(novaDiv);
+  const nomeCompleto = document.createElement('p');
+  nomeCompleto.innerHTML = `'Nome: ' + ${primeiroNome.value} + ' ' + ${sobrenome.value} + '<br>'
+  + 'Email: ' + ${emailCadastrado.value} + '<br>' + 'Casa: ' + ${casaEscolhida.value} + '<br>'
+  + 'Família: ' + ${valorDoInputRadio()} + '<br>' + 'Matérias: ' + ${valorDoInputCheckbox()}
+  + '<br>' + 'Avaliação: ' + ${valorRadioAvaliacao()} + '<br>' + 'Observações: '
+  + ${observacoes.value}`;
+  novaDiv.appendChild(nomeCompleto);
+}
+botaoEnviar.addEventListener('click', substituiFormulario);
